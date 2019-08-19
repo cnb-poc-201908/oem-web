a56fac8323309def6706d6a73562abbfd0052b2c
Konia Huo <huoqihua@cn.ibm.com>
2019-08-19 22:50:26 +0800

    back button

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
     <img class="title" src="../assets/BMW Sales.svg">
+    <Button v-if="$route.name !='home'" @click="$router.back()">返回</Button>
   </div>
 </template>
 
@@ -13,8 +14,13 @@
     components: {
       HelloWorld
     },
+    methods: {
+      goBack() {
+        this.$router.push({ path: '/' });
+      }
+    },
     created() {
-      console.log(this.$moment().format('YYYY-MM-DD'));
+      console.log(this.$route);
       // api.getData().then(res => {
       //   console.log(res.data);
       // })
@@ -24,22 +30,27 @@
 </script>
 
 <style lang="scss" scoped>
-.header {
-  height: 60px;
-  background-color: #171717;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 100;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #282828;
+  .header {
+    height: 60px;
+    background-color: #171717;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 100;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #282828;
 
-  .title {
-    width: 110px;
-    height: 30px;
-    margin-left: 30px;
+    .title {
+      width: 110px;
+      height: 30px;
+      margin-left: 30px;
+    }
+
+    button {
+      margin-right: 30px;
+    }
   }
-}
+
 </style>
